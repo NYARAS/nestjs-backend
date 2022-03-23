7ef3bad5fa1c4bfb61c24ea33a3e0eb1903556d3
Calvine Otieno <nyarangaotieno@gmail.com>
2022-03-23 12:17:07 +0300

    chore: add redis store shared module
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
     imports: [
@@ -7,6 +8,13 @@ import { JwtModule } from '@nestjs/jwt';
             secret: 'secret',
             signOptions: {expiresIn: '1d'}
           }),
+          CacheModule.register(
+              {
+                  store: redisStore,
+                  host: 'redis',
+                  port: 6379,
+              }
+          )
     ],
     exports: [JwtModule]
 })
