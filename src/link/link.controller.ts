13464c74c5cf1b7d2bd8d5afd50fd7d2d40b43ae
Calvine Otieno <nyarangaotieno@gmail.com>
2022-03-24 16:37:10 +0300

    chore: add ambassador stats
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../auth/auth.service';
 import { AuthGuard } from '../auth/auth.guard';
 import { LinkService } from './link.service';
 import { Request } from 'express';
+import { Order } from '../order/order';
+import { Link } from './link';
 
 @Controller()
 export class LinkController {
@@ -34,6 +36,27 @@ export class LinkController {
             code: Math.random().toString(36).substr(6),
             user,
             products: products.map(id => ({id}))
+        })
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('ambassador/stats')
+    async stats(@Req() request: Request) {
+        const user = await this.authService.user(request);
+
+        const links: Link[] = await this.linkService.find({
+            user,
+            relations: ['orders']
+        });
+
+        return links.map(link => {
+            const completedOrders: Order[] = link.orders.filter(o => o.complete);
+
+            return {
+                code: link.code,
+                count: completedOrders.length,
+                revenue: completedOrders.reduce((s, o) => s + o.ambassador_revenue, 0)
+            }
         })
     }
 }
